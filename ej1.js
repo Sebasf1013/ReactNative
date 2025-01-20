4458fdfe9fea42f8b1f6ddf2d5d58f7e76451ef6
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-20 17:09:48 -0500

    Añadir ejercicio 1: Declaración de variables

---
+++ ej1.js
@@ -0,0 +1,8 @@
+//Declarar las variables nombre, apellido, edad, fecha de nacimiento usando camel case (Primera letra en mayuscula en palabras pegadas) y dirección 
+let nombre = "Sebastian";
+let apellido = "Fajardo";
+let edad = 24;
+let fechaDeNacimiento = "1999-10-13";
+let direccion = "Calle 61 # 56, Medellín, Colombia";
+
+console.log(nombre, apellido, edad, fechaDeNacimiento, dirección);
