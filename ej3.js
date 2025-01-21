a8c25cca06ac863e9cda3a2bbfe8ec00cb64407e
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-21 09:47:59 -0500

    Ejercicio3: Declaración y asignacion de variables

---
+++ ej3.js
@@ -0,0 +1,15 @@
+//Declarar las siguientes variables de forma individual: nombre, apellido, edad, fecha de nacimiento, direccion. 
+//Asignar un valor a cada variable con tus datos personales: Tu nombre, tu apellido, tu edad, tu fecha de nacimiento, tu direccion
+var nombre;
+var apellido;
+var edad;
+var fechaDeNacimiento;
+var direccion;
+
+nombre = "Sebastian";
+apellido = "Fajardo";
+edad = 24;
+fechaDeNacimiento = "1999-10-13";
+direccion = "Calle 61 # 56, Medellín, Colombia";
+
+console.log(nombre, apellido, edad, fechaDeNacimiento, direccion);
