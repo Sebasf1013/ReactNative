3cc6cf44e237dd8037711937f2309aedf7b2c454
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-21 12:39:05 -0500

    Ejercicio8: Declaracion y asignacion de variables, visualizacion en consola con cada variable y su valor usando const

---
+++ ej8.js
@@ -0,0 +1,11 @@
+//Declarar las siguientes variables de forma individual utilizando <const> y asignarle a cada una un valor para describirte: nombre, apellido, edad, fecha de nacimiento, direccion
+//Mostrar en consola el valor y nombre de cada una de las variables utilizando console.log()
+const nombre = "Sebastian";
+const apellido = "Fajardo";
+const edad = 24;
+const fechaDeNacimiento = "1999-10-13";
+const direccion = "Calle 61 # 56, Medellín, Colombia";
+
+console.log("Nombre:", nombre, "y apellido:",apellido); 
+console.log("Mi fecha de nacimiento es:", fechaDeNacimiento, "y mi edad es:", edad);  
+console.log("Vivo en la calle:", direccion); 
