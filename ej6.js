c726fd035587d80e40235831a78202bfaa1e4de1
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-21 12:18:52 -0500

    Ejercicio6: Declaracion y asignacion de variables, visualizacion en consola con cada variable y su valor

---
+++ ej6.js
@@ -0,0 +1,13 @@
+//Declarar las siguientes variables de forma individual y asignarle a cada una un valor para describirte: nombre, apellido, edad, fecha de nacimiento, direccion
+//Mostrar en consola el valor y nombre de cada una de las variables utilizando console.log()
+var nombre = "Sebastian";
+var apellido = "Fajardo";
+var edad = 24;
+var fechaDeNacimiento = "1999-10-13";
+var direccion = "Calle 61 # 56, Medellín, Colombia";
+
+console.log("Nombre:", nombre); 
+console.log("Apellido:", apellido); 
+console.log("Edad:", edad); 
+console.log("Fecha de nacimiento:", fechaDeNacimiento); 
+console.log("Direccion:", direccion); 
