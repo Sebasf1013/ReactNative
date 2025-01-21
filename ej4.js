65c7bdd65f1331fef8f6d74fc924f304dbb72ca9
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-21 10:01:18 -0500

    Ejercicio4: Declaración y asignacion de variables ahí mismo

---
+++ ej4.js
@@ -0,0 +1,8 @@
+// Declarar las siguientes variables de forma individual y asignarle a cada una un valor para describirte: nombre, apellido, edad, fecha de nacimiento, direccion
+var nombre = "Sebastian";
+var apellido = "Fajardo";
+var edad = 24;
+var fechaDeNacimiento = "1999-10-13";
+var direccion = "Calle 61 # 56, Medellín, Colombia";
+
+console.log(nombre, apellido, edad, fechaDeNacimiento, direccion);
