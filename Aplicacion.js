e32913635781dfcd0e184a9ef96d4ae76efc121e
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-21 09:37:43 -0500

    Update Aplicacion.js

--- Aplicacion.js
+++ Aplicacion.js
@@ -3,4 +3,4 @@ console.log("Hola mundo");
 //Para subirlo desde visual a GitHub: 
 //git init
 //git add ej1.js
-//git commit -m "Añadir ejercicio 1: Declaración de variables"
+//git commit -m "Ejercicio#: Funcion"
