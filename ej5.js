17824354e0b693cb7964f9c37f066b5d20986d37
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-21 12:08:25 -0500

    Ejercicio5: Declaración y asignacion de variables ahí mismo y se muestra en la consola el valor de cada variable

---
+++ ej5.js
@@ -0,0 +1,10 @@
+//Declarar las siguientes variables de forma individual y asignarle a cada una un valor para describirte: nombre, apellido, edad, fecha de nacimiento, direccion
+//Mostrar en consola el valor de cada una de las variables utilizando console.log()
+var nombre = "Sebastian";
+var apellido = "Fajardo";
+var edad = 24;
+var fechaDeNacimiento = "1999-10-13";
+var direccion = "Calle 61 # 56, Medellín, Colombia";
+
+//Mostrar en consola el valor de cada una de las variables utilizando console.log()
+console.log(nombre, apellido, edad, fechaDeNacimiento, direccion);
