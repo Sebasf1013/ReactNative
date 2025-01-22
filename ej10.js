ce91b690fda9995a3ec4c67c7b9743c557401064
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-22 14:58:57 -0500

    Ejercicio10: guardar tipo de dato string

---
+++ ej10.js
@@ -0,0 +1,3 @@
+//Declarar variable saludo, asignar el valor: ECMAScript rocks!! a la variable saludo, con el tipo de dato string, mostrar el contenido de la variable con console.log()
+const saludo = "ECMAScript roks!!";
+console.log(saludo);
