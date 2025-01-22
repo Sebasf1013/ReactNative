37369494d95bd00867be95b453ff9b3fdb7f183d
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-22 14:55:15 -0500

    Ejercicio9: Reasignar una variable definida como const, no se puede

---
+++ ej9.js
@@ -0,0 +1,7 @@
+//Crear variable nombre como contante y asignarle mi nombre, luego asignarse a esa misma variable otro valor
+const nombre = "Sebastian"; 
+
+nombre = "Juan"; 
+
+console.log(nombre); 
+//Me sale error, porque estoy intentando reasignar un valor a una variable constante, lo cual no está permitido en JavaScript
