4100af2944ac3cb073857fae5d835f843f303af3
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-22 18:32:18 -0500

    Ejercicio26: Concatenar y mostrar tipo de variable relacionadas al curso

---
+++ ej26.js
@@ -0,0 +1,24 @@
+var nombreDelCurso = "React Native 1C";
+var horaInicio = "6 pm Colombia";
+var horaSalida = "9 pm Colombia";
+var nombreProfesor = "Nicolas Isnardi";
+var cantidadDeAlumnos = 18;
+var temaDelCurso = "JavaScript";
+
+console.log("\n");
+console.log("Valor de la variable");
+console.log("Nombre del curso:", nombreDelCurso);
+console.log("Hora de inicio:", horaInicio);
+console.log("Hora de finalización:", horaSalida);
+console.log("Nombre del profesor:", nombreProfesor);
+console.log("Cantidad de alumnos:", cantidadDeAlumnos);
+console.log("Tema del curso:", temaDelCurso);
+
+console.log("\n");
+console.log("información del tipo de caracter");
+console.log("Nombre del curso:", typeof nombreDelCurso);
+console.log("Hora de inicio:", typeof horaInicio);
+console.log("Hora de finalización:", typeof horaSalida);
+console.log("Nombre del profesor:", typeof nombreProfesor);
+console.log("Cantidad de alumnos:", typeof cantidadDeAlumnos);
+console.log("Tema del curso:", typeof temaDelCurso);
