0cdf72cc922cf2472fc64d1555b923b6876a441a
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-22 18:03:58 -0500

    Ejercicio25: Concatenar y mostrar tipo de variable

---
+++ ej25.js
@@ -0,0 +1,39 @@
+// Vamos a definir un jugador de un juego de super heroes utilizando los siguientes nombres de variables y valores. Utilizar los tipos de datos correspondientes para cada variable
+// nombreDeSuperHeroe: Batman
+// nombre: Bruce Wayne
+// vuela: no
+// maneja: si
+// vida: (valor de 0 a 100)
+// trabajo: indefinido
+// tiempoLibre: nulo
+// Mostrar el valor de las variables de la siguiente forma: console.log(nombreDeLaVariable, valor)
+// Mostrar el tipo de dato de las variables de la siguiente forma: console.log(nombreDeLaVariable, typeof nombreDeLaVariable)
+
+const nombreDeSuperHeroe = "Batman";
+const nombre = "Bruce Wayne";
+const vuela = "no";
+const maneja = "si";
+const vida = 100;
+const trabajo = undefined;
+const tiempoLibre = null;
+
+console.log("\n");
+console.log("Valor de la variable");
+console.log("nombreDeSuperHeroe: ", nombreDeSuperHeroe);
+console.log("nombre: ", nombre);
+console.log("vuela: ", vuela);
+console.log("maneja: ", maneja);
+console.log("vida: ", vida);
+console.log("trabajo: ", trabajo);
+console.log("tiempoLibre: ", tiempoLibre);
+
+console.log("\n");
+console.log("información del tipo de caracter");
+console.log("nombreDeSuperHeroe: ", typeof nombreDeSuperHeroe);
+console.log("nombre: ", typeof nombre);
+console.log("vuela: ", typeof vuela);
+console.log("maneja: ", typeof maneja);
+console.log("vida: ", typeof vida);
+console.log("trabajo: ", typeof trabajo);
+console.log("tiempoLibre: ", typeof tiempoLibre);
+
