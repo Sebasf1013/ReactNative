f977193a150c1cec90f782ab7cf3ce37544d29f8
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-22 16:40:04 -0500

    Ejercicio21: Concatenar datos

---
+++ ej21.js
@@ -0,0 +1,5 @@
+var edad = "25"; 
+var telefono = "3132564376"; 
+var calle = "Calle"
+var altura = "61"; 
+console.log("Tengo",edad,"años","\n","Mi telefono es",telefono,"\n","Vivo en la",calle,altura);
