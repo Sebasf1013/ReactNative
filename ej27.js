d901e59a922f0a5afaf577bb628af3babb94c3ae
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-22 19:35:51 -0500

    Ejercicio27: Operaciones matemáticas

---
+++ ej27.js
@@ -0,0 +1,24 @@
+// Declarar 2 variables con los nombres numero1 y numero2
+// Asignarle a cada variable un valor del tipo number
+// Para cada operación mostrar el mensaje de la siguiente forma:
+
+// Ejemplos: sumo 2 + 3 y el resultado es 5 (donde 2 y 3 son los valores de las variables y 5 es el resultado de la operación)
+// Mostrar en consola el resultado de la suma de las dos variables
+// Mostrar en consola el resultado de la resta de las dos variables
+// Mostrar en consola el resultado de la multiplicación de las dos variables
+// Mostrar en consola el resultado de la división de las dos variables
+// Mostrar en consola el resultado del resto de las dos variables
+
+const numero1 = 3; 
+const numero2 = 2; 
+const suma = numero1 + numero2; 
+const resta = numero1 - numero2; 
+const multiplicacion = numero1 * numero2; 
+const division = numero1 / numero2; 
+const residuo = numero1 % numero2; 
+console.log("La suma de", numero1, "+", numero2, "es:", suma);
+console.log("La resta de", numero1, "-", numero2, "es:", resta);
+console.log("La multiplicacion de", numero1, "*", numero2, "es:", multiplicacion);
+console.log("La division de", numero1, "/", numero2, "es:", division);
+console.log("El residuo de", numero1, "%", numero2, "es:", residuo);
+
