507f290268f4d375b01a07fcb54979f5a1ee6b7f
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-22 19:47:32 -0500

    Ejercicio29: Operaciones perimetro de un cuadrado

---
+++ ej29.js
@@ -0,0 +1,4 @@
+// Calcular y mostrar en consola el perímetro de un cuadrado (el perímetro es simplemente cuatro veces la longitud del lado)
+// La longitud del lado es de 10
+const lado = 10;
+console.log("El perimetro del cuadrado de longitud de lado 10 es: ", lado * 4);
