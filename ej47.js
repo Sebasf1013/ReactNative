0e9b1553da4e5f60b7380aca7d43d74d526f3902
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 16:23:38 -0500

    Ejercicio 47: Uso de .split para dividir cadena de texto a arreglo

---
+++ ej47.js
@@ -0,0 +1,7 @@
+// Declarar la variable alumnos y asignar el siguiente texto: Matías, Germán, Nicolás, Silvia, Estefi, Patricia, Ramóm
+// Asignar a la variable alumnos el resultado de ejecutar el método split sobre la variable alumnos
+// Mostrar por consola el contenido de la variable alumnos
+
+var alumnos = "Matías, Germán, Nicolás, Silvia, Estefi, Patricia, Ramón";
+alumnos = alumnos.split(", ");
+console.log(alumnos);
