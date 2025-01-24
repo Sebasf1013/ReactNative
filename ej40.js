f07162ab1e5dcadff934a2ab43a99e81f15b14ed
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-23 20:52:51 -0500

    Ejercicio40: Concatenar usando el método concat

---
+++ ej40.js
@@ -0,0 +1,10 @@
+// Declarar la variable texto con el siguiente valor: Usando el método
+// Declarar la variable metodo con el siguiente valor: concat
+// Mostrar en consola el siguiente texto utilizando el método de string concat: Usando el método concat
+
+var texto = "Usando el método"; 
+var metodo = "concat"
+console.log(texto,"de string",metodo,":",texto,metodo); //Lo hice mal porque pense que ese era el texto a mostrar 
+
+//Otra forma: 
+console.log(texto.concat(" ", metodo));
