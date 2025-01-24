8d301ee162977afdf371985fa4591a40cf14ee8d
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 17:12:06 -0500

    Ejercicio 56: uso de refactorizar

---
+++ ej56.js
@@ -0,0 +1,13 @@
+// Copiar el código del ej53.md
+// Refactorizar el código y utilizar operador ternario en lugar de if/else
+const numero = 10;
+
+(numero % 2 == 0) ? console.log("El número es par") : console.log("El número no es par");
+
+//if/else
+if(numero % 2 == 0){
+    console.log("El número es par");
+}
+else {
+    console.log("El número no es par");
+}
