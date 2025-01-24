992e7af70259688784a48e054d73ef99a01196a2
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-23 20:11:29 -0500

    Ejercicio35: Operaciones booleanas con typeof

---
+++ ej35.js
@@ -0,0 +1,13 @@
+// Declar la variable number numeroDeCliente y asignar el valor 143245
+// Declar la variable numeroDeClienteIngresado y asignar el valor '143245'
+// Mostrar en consola si el dato ingresado y el número de cliente son iguales (comparación por valor)
+// Utilizar el siguiente mensaje: El número ingresado por el cliente es correcto: true o false
+// Mostrar en consola si el dato ingresado y el número de cliente son iguales (comparación por valor y tipo de dato)
+// Utilizar el siguiente mensaje: El cliente ingresó un número de cliente válido: true o false
+let numeroDeCliente = 143245; 
+let numeroDeClienteIngresado = 143245 
+let comparar = numeroDeCliente == numeroDeClienteIngresado;
+console.log("El número ingresado por el cliente es correcto:", comparar);
+let compararType = typeof numeroDeCliente == typeof numeroDeClienteIngresado;
+console.log("El cliente ingresó un número de cliente válido:", comparar);
+
