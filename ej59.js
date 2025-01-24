949edd103a9b5343c8bd00333664fbfce8698315
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 17:23:15 -0500

    Ejercicio 59: Uso de if/else if/else, para numeros

---
+++ ej59.js
@@ -0,0 +1,22 @@
+// Declarar la variable numero1 y numero2
+// Asignar un valor numérico a cada variable declarada
+// Si la variable numero1 y numero2 son iguales entonces
+// Mostrar el siguiente mensaje en consola: Los dos números son iguales
+// Si la variable numero1 es más grande que numero2 entonces
+// Mostrar el siguiente mensaje en consola: La variable numero1 es más grande que numero2
+// Si No
+// Mostrar el siguiente mensaje en consola: La variable numero2 es más grande que numero1
+// Cambiar el valor de la variable hora un par de veces y ejecutarlo para ver que pasa
+// Probar cambiar los mensajes utilizando los valores reales de cada variable
+
+const numero1 = 10; 
+const numero2 = 15; 
+if (numero1 == numero2){
+    console.log("Son iguales ambos numeros");
+}
+else if (numero1 > numero2){
+    console.log("numero1 es mas grande que numero2");
+}
+else {
+    console.log("numero2 es más grande que numero1");
+}
