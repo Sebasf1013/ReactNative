f51008f1271ed09f56a47967c7a7aa4d171abd1d
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 16:42:19 -0500

    Ejercicio 50: Uso del modulo % para decir que un numero es par porque es el residuo

---
+++ ej50.js
@@ -0,0 +1,8 @@
+// Declarar la variable numero y asignar el valor 10
+// Si la variable numero es par Entonces mostrar en consola el mensaje: El número es par
+// Probar cambiar el valor de la variable numero por un número impar y volver a ejecutar
+
+const numero = 10;
+if(numero % 2 == 0){
+    console.log("El número es par");
+}
