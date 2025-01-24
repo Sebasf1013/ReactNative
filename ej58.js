f9c16f0d4333707410d99950b7af977ca4fe35da
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 17:20:14 -0500

    Ejercicio 58: Uso de if/else if/else, para horarios 24h

---
+++ ej58.js
@@ -0,0 +1,18 @@
+// Declarar la variable hora
+// Establecer un valor entre 0 y 24
+// Si la hora es menor que 12 y mayor que 5 entonces
+// Mostrar el mensaje: Buen día
+// Si la hora es mayor o igual que 12 y menor que 20 entonces
+// Mostrar el mensaje: Buenas tardes
+// Si No:
+// Mostrar el mensaje: Buenas noches
+// Cambiar el valor de la variable hora un par de veces y ejecutarlo para ver que pasa
+
+const hora = 24; 
+if(hora < 12 && hora > 5){
+    console.log("Buen dia");
+}else if(hora >= 12 && hora <= 20){
+    console.log("Buenas tardes");
+}else{
+    console.log("Buenas noches");
+}
