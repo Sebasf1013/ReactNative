cf3d4ba98cc0d039fd69f6cd6a493a0ab8c42c62
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 12:41:54 -0500

    Ejercicio 41: Minusculas en todo el texto

---
+++ ej41.js
@@ -0,0 +1,4 @@
+// Declarar la variable texto y asignar el siguiente valor: HOLA MUNDO
+// Mostrar en consola el contenido de la variable texto en minúscula
+var texto = "HOLA MUNDO"; 
+console.log(texto.toLowerCase()); 
