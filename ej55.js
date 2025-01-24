6c5c03e119f3f31465cb74f47e1e1529634ba8ff
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 17:10:49 -0500

    Ejercicio 55: uso de refactorizar en lugar de if/else

---
+++ ej55.js
@@ -0,0 +1,14 @@
+// Copiar el código del ej52.md
+// Refactorizar el código y utilizar operador ternario en lugar de if/else
+const numero1 = 10; 
+const numero2 = 5; 
+
+(numero1 > numero2) ? console.log("La variable numero1 es mas grande que la variable numero2") : console.log("La variable numero1 no es más grande que la variable numero2"); 
+
+//Otra forma
+if (numero1 > numero2) {
+    console.log("La variable numero1 es mas grande que la variable numero2");
+}
+else {
+    console.log("la variable numero1 no es más grande que la variable numero2");
+}
