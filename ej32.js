2f460fa0758410e543217592b6693cbb635f8c5a
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-23 19:37:33 -0500

    Ejercicio32: Operacion perimetro y base de un rectangulo

---
+++ ej32.js
@@ -0,0 +1,10 @@
+// Declarar la variable altura y asignar el valor 10
+// Declarar la variable base y asignar el valor 4
+// Mostrar en consola el cálculo del perímetro de un rectangulo (suma de los lados) y el área (base por altura).
+
+const altura = 10; 
+const base = 4; 
+const perimetro = altura + base; 
+const area = base * altura; 
+
+console.log(`El perimetro del rectangulo de altura ${altura} y base ${base} es: ${perimetro}. El area es ${area}`);
