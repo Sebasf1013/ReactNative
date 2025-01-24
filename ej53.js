5cb7d25c0af8d727236ff42eda9d12147450cd23
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 17:07:33 -0500

    Ejercicio 53: Uso de if y else en numeros

---
+++ ej53.js
@@ -0,0 +1,14 @@
+// Copiar el código del ej50.md
+// Modificar el código para que cumpla con el nuevo requisito
+// Si la variable numero es par Entonces
+// Mostrar en consola el mensaje: El número es par
+// SI NO :
+// Mostrar en consola el mensaje: El número no es par
+
+const numero = 10;
+if(numero % 2 == 0){
+    console.log("El número es par");
+}
+else {
+    console.log("El número no es par");
+}
