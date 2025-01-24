1a3813cc89b522c31bc60bb5fb96a07d2dc0fed8
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 17:01:50 -0500

    Ejercicio 52: Uso de If y else

---
+++ ej52.js
@@ -0,0 +1,16 @@
+// Copiar el código del ej49.md
+// Modificar el código para que cumpla con el nuevo requisito
+// Si la variable numero1 es más grande que la variable numero2 entonces
+// Mostrar el siguiente mensaje en consola: La variable numero1 es más grande que la variable numero2
+// SI NO :
+// Mostrar el siguiente mensaje: La variable numero1 no es más grande que la variable numero2
+// Probar diferentes valores para ver los diferentes mensajes
+
+const numero1 = 10; 
+const numero2 = 5; 
+if (numero1 > numero2) {
+    console.log("La variable numero1 es mas grande que la variable numero2");
+}
+else {
+    console.log("la variable numero1 no es más grande que la variable numero2");
+}
