bb62d6046e5d800536b0e4ea9f6a42a171ed1486
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-23 20:24:18 -0500

    Ejercicio36: Igualdad y desigualdad

---
+++ ej36.js
@@ -0,0 +1,14 @@
+// Declarar las siguientes variables numero1 y numero2
+// Asignar a numero1 el valor 10
+// Asignar a numero2 el valor 20
+// Mostrar en consola los siguientes mensajes:
+// La variable numero1 y numero2 tienen el mismo valor: true/false
+// La variables numero1 y numero2 son distintas: true/false
+
+let numero1 = 10; 
+let numero2 = 20; 
+let igualdad = numero1 == numero2; 
+let desigualdad = numero1 !== numero2; 
+
+console.log("La variable numero1 y numero2 tienen el mismo valor:", igualdad);
+console.log("La variable numero1 y numero2 son distintas:", desigualdad);
