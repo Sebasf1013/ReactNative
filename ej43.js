f7abdfcdfd46a8988b2900b530fa30a111144433
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-24 12:56:29 -0500

    Ejercicio 43: Extraer posición de texto con charAt

---
+++ ej43.js
@@ -0,0 +1,19 @@
+// Declarar una variable texto con el siguiente valor: game of thrones
+// Declarar 3 variables en una línea con los siguientes nombres:
+// primerLetra
+// segundaLetra
+// tercerLetra
+// Asignar a la variable primerLetra el primer caracter del contenido de la variable texto
+// Asignar a la variable segundaLetra el caracter número 5 del contenido de la variable texto
+// Asignar a la variable tercerLetra el caracter número 8 del contenido de la variable texto
+// Declarar una variable resultado con el valor de concatenar las variables primerLetra, segundaLetra y tercerLetra
+// Mostrar en consola el contenido de la variable resultado en mayúscula
+// Deberías ver GOT como resultado
+
+var texto = "game of thrones"; 
+let primerLetra, segundaLetra, tercerLetra;
+primerLetra = texto.charAt(0);
+segundaLetra = texto.charAt(5); 
+tercerLetra = texto.charAt(8);
+var resultado = primerLetra.concat(segundaLetra,tercerLetra); 
+console.log(resultado.toUpperCase());
