9af004e8dd8a667c5d57ecb8ad410bc0bc656c41
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 11:54:00 -0500

    Ejercicio72: While para dibujar piramide de *

---
+++ ej72.js
@@ -0,0 +1,8 @@
+// Mostrar en consola el siguiente dibujo utilizando la estructura whilen (desde 1 * hasta 14*)
+let numero = 1; 
+let dibujo = ""; 
+while (numero < 15){
+    dibujo += "*"; 
+    console.log(dibujo); 
+    numero++;
+}
