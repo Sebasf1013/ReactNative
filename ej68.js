4245d8f878d339c132eb8e9ed5f4ab9512ebff47
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 11:17:13 -0500

    Ejercicio68: while del 0 al 1000

--- ej68.js
+++ ej68.js
@@ -1,6 +1,6 @@
 // Mostrar en consola los números pares del 0 al 1000 utilizando la estructura while
 let numero = 0; 
-while (numero <= 1000){
+while (numero <= 10){
     console.log(numero);
     numero++;
 }
