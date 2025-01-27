991d658ccf8964093835414a3e1da337f068c471
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 16:13:45 -0500

    Ejercicio84: Do/While

---
+++ ej84.js
@@ -0,0 +1,10 @@
+// Copiar y pegar el código del ejercicio ej74.js
+// Refactorizar el código utilizando la estructura do/while en lugar de while
+
+let numero = 1;
+let dibujo = "";
+do{
+    console.log("*".repeat(numero));
+    numero+=2;
+}
+while(numero < 15);
