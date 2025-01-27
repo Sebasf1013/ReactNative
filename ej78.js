41c494cbd7f31d16e122d1b821b98005f3329884
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 15:57:46 -0500

    Ejercicio78: Do/While

---
+++ ej78.js
@@ -0,0 +1,9 @@
+// Copiar y pegar el código del ejercicio ej68.js
+// Refactorizar el código utilizando la estructura do/while en lugar de while
+let numero = 0;
+do{
+    if(numero % 2 == 0){
+        console.log(numero);
+    }
+    numero++;
+}while(numero <= 1000);
