53b05e97d1ff65189e08b561b7e95ae6ee5c4d8a
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 11:28:58 -0500

    Ejercicio69: while del 0 al 1000 con suma parcial

---
+++ ej69.js
@@ -0,0 +1,9 @@
+// Mostrar en consola la suma parcial de todos los números que van desde 0 hasta 1000 utilizando la estructura while.
+let numero = 0; 
+let numero1 = 0; 
+while (numero <= 10){
+    numero1 = numero + numero1;
+    console.log("La suma es:", numero1, "para:", numero);
+    numero++; 
+}
+
