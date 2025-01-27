91dd720f43ba5ee94fd29fe07aeb59a25bd91927
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 16:20:25 -0500

    Ejercicio88: Usando el ciclo for

---
+++ ej88.js
@@ -0,0 +1,7 @@
+// Copiar y pegar el código del ejercicio ej68.js
+// Refactorizar el código utilizando la estructura for en lugar de do/while
+for(let i = 0; i <= 1000; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
