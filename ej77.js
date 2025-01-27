f27c698761520020885e45bf2bae9db04597369b
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 15:44:12 -0500

    Ejercicio77: Do/While

---
+++ ej77.js
@@ -0,0 +1,7 @@
+// Copiar y pegar el código del ejercicio ej67.js
+// Refactorizar el código utilizando la estructura do/while en lugar de while
+let numero = 100; 
+do{
+    console.log(numero); 
+    numero--;
+} while (numero >= 0);
