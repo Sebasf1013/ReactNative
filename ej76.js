f84e4b26d94b2e4eebfebcc889796e05ff5dc95b
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 15:42:59 -0500

    Ejercicio76: Do/While... haz esto mientras

---
+++ ej76.js
@@ -0,0 +1,8 @@
+// Copiar y pegar el código del ejercicio ej66.js
+// Refactorizar el código utilizando la estructura do/while en lugar de while
+
+let numero = 0; 
+do{
+    console.log(numero);
+    numero++;
+} while (numero <= 10);
