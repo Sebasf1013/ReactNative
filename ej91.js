d3974d7932ff9ed9a14f078373df9b55f22961e6
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 16:23:43 -0500

    Ejercicio91: Usando el ciclo for

---
+++ ej91.js
@@ -0,0 +1,5 @@
+// Copiar y pegar el código del ejercicio ej71.js
+// Refactorizar el código utilizando la estructura for en lugar de do/while
+for(let i = 1; i <= 10; i++){
+    console.log(`9 x ${i} = ${i * 9}`);
+}
