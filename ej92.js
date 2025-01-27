6ff138b68305909407bb68de771ac40414ee78c9
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 16:25:32 -0500

    Ejercicio92: Usando el ciclo for

---
+++ ej92.js
@@ -0,0 +1,7 @@
+// Copiar y pegar el código del ejercicio ej72.js
+// Refactorizar el código utilizando la estructura for en lugar de do/while
+let dibujo = "";
+for(let i = 1; i < 15; i++){
+    dibujo += "*";
+    console.log(dibujo);
+}
