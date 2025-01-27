9009ae90f077f5a8c595faf55f01bad2c2023653
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 16:12:36 -0500

    Ejercicio81: Do/While

---
+++ ej81.js
@@ -0,0 +1,8 @@
+// Copiar y pegar el código del ejercicio ej71.js
+// Refactorizar el código utilizando la estructura do/while en lugar de while
+
+let numero = 1;
+do{
+    console.log(`9 x ${numero} = ${numero * 9}`);
+    numero++;
+}while(numero <= 10);
