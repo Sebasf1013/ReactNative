cc7d9c249caedcdf274f8ce5df47d68ea42f841d
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 19:48:51 -0500

    Ejercicio102: Asignar una funcion a una variable

---
+++ ej102.js
@@ -0,0 +1,15 @@
+// Declarar una variable mostrarNombre y asignar una función
+// La función mostrarNombre tiene que mostrar tu nombre en consola con el siguiente formato:
+//   ===========
+//   = Nicolás =
+//   ===========
+// Ejecutar esta función 2 veces
+
+let mostrarNombre = function(){
+    console.log("========");
+    console.log("=Sebastian=");
+    console.log("========");
+}
+
+mostrarNombre();
+mostrarNombre();
