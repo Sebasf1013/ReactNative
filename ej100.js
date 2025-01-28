b7457b2d09b497f9d142caf2fd7e5412f344f0bb
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 19:38:32 -0500

    Ejercicio100: Funciones para mostrar numeros pares del 0 al 100

---
+++ ej100.js
@@ -0,0 +1,10 @@
+// Declarar una función que muestre en consola los números pares del 0 al 100
+function numerosPares(){
+    for(i = 0; i <= 100; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+
+numerosPares();
