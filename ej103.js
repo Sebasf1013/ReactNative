bc9e5aef355ee0c80faf3680100fb115e3c509c6
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 19:51:23 -0500

    Ejercicio103: Asignar una funcion a una variable y mostrar numeros pares

---
+++ ej103.js
@@ -0,0 +1,17 @@
+// Declarar una variable numerosPares y asignar una función
+// La función numerosPares muestra en consola los números pares del 0 al 100
+// Ejecutar esta función 5 veces
+
+let numerosPares = function(){
+    for ( i = 0; i <= 100; i++){
+        if (i % 2 === 0){
+            console.log(i);
+        }
+    }
+}
+
+numerosPares();
+numerosPares();
+numerosPares();
+numerosPares();
+numerosPares();
