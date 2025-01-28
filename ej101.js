ae6f5206907862d852ab2ad0c57a241a5a9d4de5
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 19:45:01 -0500

    Ejercicio101: Funcion para mostrar datos de nombre, etc

---
+++ ej101.js
@@ -0,0 +1,28 @@
+// Declarar una función que muestre los siguientes datos en consola:
+// Tu Nombre
+// Edad
+// Teléfono
+// Calle
+// Altura
+// Código postal
+// Casado true/false
+
+function datos() {
+    const nombre = "Sebastian";
+    const edad = "25 años";
+    const telefono = "3132564376";
+    const calle = "61";
+    const altura = "56";
+    const codigoPostal = "00506";
+    const casado = false;
+
+    console.log("Nombre:", nombre);
+    console.log("Edad:", edad);
+    console.log("Teléfono:", telefono);
+    console.log("Calle:", calle);
+    console.log("Altura:", altura);
+    console.log("Código Postal:", codigoPostal);
+    console.log("Casado:", casado);
+}
+
+datos();
