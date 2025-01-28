6f08d49852015fda08647053e87ad24ae312d308
Sebasf1013 <120421798+Sebasf1013@users.noreply.github.com>
2025-01-27 19:56:41 -0500

    Ejercicio105: Funcion para retornar con return un valor

---
+++ ej105.js
@@ -0,0 +1,9 @@
+// Crear una función que se llame significadoDeLaVida y retorna el número 42
+// Mostrar en consola el siguiente mensaje: El significado de la vida es ${significadoDeLaVida}
+
+let significadoDeLaVida = function(){
+    let numero = 42;
+    return(numero);
+}
+
+console.log("El significado de la vide es",significadoDeLaVida());
